c14bc6998ac6f49649c1ea25895abe39bb9689fc
Craig Miller <craigmiller160@gmail.com>
2021-11-03 16:43:57 -0400

    Added mineBlock function

--- src/Block.ts
+++ src/Block.ts
@@ -5,8 +5,9 @@ import { format } from 'date-fns';
 const TIMESTAMP_FORMAT = 'yyyyMMddHHmmssSSS';
 
 export class Block<D> {
-    readonly hash: string;
+    hash: string; // TODO make it readonly again
     readonly timestamp: string;
+    nonce: number = 0; // TODO make it readonly
 
     constructor(
         public readonly data?: D,
@@ -19,11 +20,19 @@ export class Block<D> {
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.nonce + this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 
     withIndexAndPreviousHash(index: number, previousHash: string): Block<D> {
         return new Block(this.data, index, previousHash, this.timestamp);
+    }
+
+    mineBlock(difficulty: number) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.hash = this.calculateHash();
+            this.nonce++;
+        }
+        console.log(`Block mined: ${this.hash}`);
     }
 
 }

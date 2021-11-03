f417646c7b6e1f8d37e90daab703a4999752982a
Craig Miller <craigmiller160@gmail.com>
2021-11-03 16:47:06 -0400

    Made chain property private

--- src/Blockchain.ts
+++ src/Blockchain.ts
@@ -1,36 +1,36 @@
 import { Block } from './Block';
 
 export class Blockchain<D> {
-    private chain: ReadonlyArray<Block<D>>;
+    #chain: ReadonlyArray<Block<D>>;
     constructor(existingChain?: ReadonlyArray<Block<D>>) {
-        this.chain = existingChain ?? [new Block()];
+        this.#chain = existingChain ?? [new Block()];
     }
 
     getLatestBlock(): Block<D> {
-        return this.chain[this.chain.length - 1];
+        return this.#chain[this.#chain.length - 1];
     }
 
     getChain(): ReadonlyArray<Block<D>> {
-        return this.chain;
+        return this.#chain;
     }
 
     addBlock(newBlock: Block<D>) {
-        const index = this.chain.length;
+        const index = this.#chain.length;
         const newBlockWithPreviousHash = newBlock.withIndexAndPreviousHash(index, this.getLatestBlock().hash);
-        this.chain = [
-            ...this.chain,
+        this.#chain = [
+            ...this.#chain,
             newBlockWithPreviousHash
         ];
     }
 
     isChainValid(): boolean {
-        return this.chain.slice(1)
+        return this.#chain.slice(1)
             .reduce((status: boolean, block, index, currentChain) => {
                 if (!status) {
                     return status;
                 }
                 // 'index' in reduce after slice is actually 'index - 1' on the base chain
-                const prevBlock = this.chain[index];
+                const prevBlock = this.#chain[index];
 
                 return block.hash === block.calculateHash() &&
                     block.previousHash === prevBlock.hash;

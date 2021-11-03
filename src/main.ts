66e719339a575efd40a90a3e3a88a9e10ce5d654
Craig Miller <craigmiller160@gmail.com>
2021-11-03 17:26:41 -0400

    Fixed all remaining compile errors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { Blockchain } from './Blockchain';
 import { Block } from './Block';
-import { MyCoin } from './MyCoin';
 
-const myCoinBlockChain = new Blockchain<MyCoin>();
-myCoinBlockChain.addBlock(Block.fromTransactions<MyCoin>([{ amount: 4 }]));
-myCoinBlockChain.addBlock(Block.fromTransactions<MyCoin>([{ amount: 10 }]));
+const myCoinBlockChain = new Blockchain();
+// myCoinBlockChain.addBlock(Block.fromTransactions<MyCoin>([{ amount: 4 }]));
+// myCoinBlockChain.addBlock(Block.fromTransactions<MyCoin>([{ amount: 10 }]));
 
 console.log(JSON.stringify(myCoinBlockChain.getChain(), null, 2));
 console.log(`Is Chain Valid: ${myCoinBlockChain.isChainValid()}`);
